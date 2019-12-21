6759feb67560244582d4a7e4e0882851e9c3766a
YuMingChang <32900022+YumingChang02@users.noreply.github.com>
2019-12-21 21:35:50 +0800

    Halide_mm
    Notice : RDom needs to differ with input

--- halide_mm/mm_generator.cpp
+++ halide_mm/mm_generator.cpp
@@ -1,4 +1,3 @@
-
 #include "Halide.h"
 #include <stdio.h>
 #include "mm.h"
@@ -6,7 +5,7 @@
 using namespace Halide;
 using namespace Halide::ConciseCasts;
 
-class HalideMM : public Generator<HalideMM> 
+class HalideMM : public Generator<HalideMM>
 {
 public:
     Input<Buffer<>> ma{"MA", 2};
@@ -15,15 +14,23 @@ public:
     Output<Func> output{"output", 2};
 
     void generate(){
+    RDom r ( 0, k );
+    output ( x, y ) = sum( cast( output.type(), ma( r, y ) ) * cast( output.type(), mb( x, r )  ) );
     }
 
     void schedule(){
         if(auto_schedule){
             // 1. Buffer : buf.dim(N).set_estimate(MIN, EXTENT)
-
+            ma.dim(0).set_estimate(0,512);
+            ma.dim(1).set_estimate(0,512);
+            mb.dim(0).set_estimate(0,512);
+            mb.dim(1).set_estimate(0,512);
             // 2. parameters : parm.set_estimate(VALUE)
+            k.set_estimate(512);
 
             // 3. Func : func.set_estimate(Var, MIN, EXTENT);
+            output.set_estimate(x,0,512);
+            output.set_estimate(y,0,512);
         }
         else{
         }
@@ -32,4 +39,3 @@ private:
     Var x, y;
 };
 HALIDE_REGISTER_GENERATOR(HalideMM, halide_mm);
-
